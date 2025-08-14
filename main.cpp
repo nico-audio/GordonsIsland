1bb9c6d91b4954ae3bbd44f805c869579dbf4a96
Nico <nicovieira.dev@gmail.com>
2025-08-14 19:56:01 +0100

    feat: idle animation, run animation

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  * Description: [Top down 2D game]
  * Author:      [Nico V.]
  * Created on:  [11/08/2025]
- * Last updated:[12/08/2025, Movement implementation, add character texture]
+ * Last updated:[14/08/2025, Idle animation, run animation]
  * Version:     [0.0.1]
  *
  * Notes:
@@ -49,15 +49,31 @@
 
     // Load character texture
     Texture2D gordon = LoadTexture("characters/Gordon/Unarmed_Idle/Unarmed_Idle_full.png");
+    Texture2D gordonIdle = LoadTexture("characters/Gordon/Unarmed_Idle/Unarmed_Idle_full.png");
+    Texture2D gordonRun = LoadTexture("characters/Gordon/Unarmed_Run/Unarmed_Run_full.png");
     Vector2 gordonPos{
         (float)kWindowWidth/2.0f - 4.0f * (0.5f * (float)gordon.width/12.0f),
         (float)kWindowHeight/2.0f - 4.0f * (0.5f * (float)gordon.height/4.0f)
     };
+
+    // 1: facing right, -1: facing left
+    float rightLeft{1.f};
+
+    // Animation variables
+    float runningTime{};
+    int frame{};
+    const int kMaxFrames{12};
+    const float kUpdateTime{1.f/12.f};
+
+    float sprSize{12};
     
     SetTargetFPS(60);
     
     // Keep window open and clear background
     while (!WindowShouldClose()){
+
+        // Delta time
+        const float dT{GetFrameTime()};
         
         // Start drawing
         BeginDrawing();
@@ -67,14 +83,35 @@
         Vector2 direction = getInputDirection();
         if (Vector2Length(direction) != 0.0){
             worldMapPos = Vector2Subtract(worldMapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            rightLeft = ((direction.x < 0.f) ? -1.f : 1.f);
+            gordon = gordonRun;
+            sprSize = 8.f;
+        }
+        else{
+            gordon = gordonIdle;
+            sprSize = 12.f;
         }
 
         // Draw world map
         DrawTextureEx(worldMap, worldMapPos, 0.0, 4.0, WHITE);
 
+        // Update running time
+        runningTime += dT;
+        
+        // Update animation frame
+        if(runningTime >= kUpdateTime){
+            runningTime = 0.0;
+            frame++;
+            if(frame > kMaxFrames) frame = 0;
+        }
+
         // Draw character
-        Rectangle source{0.f, 0.f, (float)gordon.width/12.f, (float)gordon.height/4.0f};
-        Rectangle destRec{gordonPos.x, gordonPos.y, 4.0f * (float)gordon.width/12.0f, 4.0f * (float)gordon.height/4.0f};
+        Rectangle source{frame * (float)gordon.width/sprSize, 0.f, (float)gordon.width/sprSize, (float)gordon.height/4.0f};
+        Rectangle destRec{gordonPos.x, gordonPos.y, 4.0f * (float)gordon.width/sprSize, 4.0f * (float)gordon.height/4.0f};
+
+        // Select different row from spritesheet
+        source.y = ((rightLeft == 1.f) ? 2.f* source.height : 1.f * source.height);
+
         DrawTexturePro(gordon, source, destRec, Vector2{}, 0.f, WHITE);
 
         // Stop drawing
@@ -82,5 +119,6 @@
     }
 
     UnloadTexture(worldMap);
+    UnloadTexture(gordon);
     CloseWindow();
 }
